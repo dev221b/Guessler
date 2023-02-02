40adbcfd42b22251bb5ea5dbbbb3be0375620e82
Devika <devikab2021@gmail.com>
2023-02-02 12:49:42 +0530

    Working modified

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ const dict = {
                 "Went to Harvard Law School",
                 "Honoured with the Nobel Peace Prize 2009",
                 "44th President of US",
-                "First African-American President"],
+                "First African-American President",
+                "obama.jpeg"
+            ],
     "gandhi" : ["Father of India",
                 "Dandi March",
                 "42nd Round Table Conference",
                 "Ahimsa",
                 "Hai Ram",
                 "Not related to Indira Gandhi",
-                "Freedom Fighter"]
+                "Freedom Fighter",
+                "gandhi.jpeg"            
+            ]
             }
 const people = ["obama", "gandhi"]
 const person = people[1]
@@ -21,8 +25,24 @@ for (var i = 0; i<dict[person].length;i++){
     hints[i] = dict[person][i]
 }
 const alertContainer = document.querySelector("[data-alert-container]")
-document.getElementById("1").innerHTML = hints[0]
 var counter = 1
+// const firstHint = document.getElementById("1")
+// danceHint(firstHint)
+// firstHint.innerHTML = hints[0]
+window.onload = () => {
+    setTimeout(() => {
+        const firstHint = document.getElementById("1")
+        danceHint(firstHint)
+        firstHint.innerHTML = hints[0]
+    }, 2000); 
+    // ( () => {
+    //     const firstHint = document.getElementById("1")
+    //     danceHint(firstHint)
+    //     setTimeout( () => {
+    //         firstHint.innerHTML = hints[0]
+    //     }, 500)
+    // }, 500)
+}
 
 startInteraction()
 
@@ -46,19 +66,26 @@ function submitGuess() {
     const guess = document.getElementById("guess").value
     console.log(guess)
     console.log(person)
-    if (guess === person) {
+    if (guess.localeCompare(person) == 0) {
+        document.getElementById("pic").src = hints[7]
         showAlert("You Win")
+        stopInteraction()
         return
     }
     else {
+        
         if(counter >= 7){
             stopInteraction()
+            document.getElementById("pic").src = hints[7]
             showAlert(person)
         }
         counter = counter + 1
         const index = counter.toString()
         console.log(index)
-        document.getElementById(index).innerHTML = addHint(index)
+        const hinter = document.getElementById(index)
+        hinter.innerHTML  = addHint(index)
+        hinter.innerHTML.animation = 
+        danceHint(hinter)
         startInteraction()
     }
 }
@@ -68,7 +95,7 @@ function addHint(i) {
     return s
 }
 
-function showAlert(message, duration = 1000) {
+function showAlert(message, duration = 5000) {
     const alert = document.createElement("div")
     alert.textContent = message
     alert.classList.add("alert")
@@ -81,3 +108,24 @@ function showAlert(message, duration = 1000) {
         })
     }, duration)
 }
+
+function danceHint(box) {
+    // console.log("Dance")
+    //     setTimeout(() => {
+    //         // box.style.animation = "dance 1s 1"
+    //         box.classList.add("dance")
+    //         box.addEventListener("animationend", () => {
+    //         box.classList.remove("dance")
+    //         }, {once: true})
+            
+    //     }, 200 )
+
+    
+        setTimeout(() => {
+            console.log("Dance")
+            box.classList.add("dance")
+            box.addEventListener("animationend", () => {
+            box.classList.remove("dance")
+        }, {once: true})
+        }, 100 )
+    }
